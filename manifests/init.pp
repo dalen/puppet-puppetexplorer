47e2d2a85bb6106e5f28c81d858614013901ea59
Luca Lesinigo <luca@lm-net.it>
2015-05-18 12:36:26 +0200

    use full APT key fingerprint instead of short key ID
    Using the full fingerprint closes a security issue and prevents the
    puppetlabs/apt module from issuing security warnings at every agent run.

--- manifests/init.pp
+++ manifests/init.pp
@@ -147,7 +147,7 @@ class puppetexplorer (
       location    => 'http://apt.puppetexplorer.io',
       release     => 'stable',
       repos       => 'main',
-      key         => '3FF5E93D',
+      key         => 'CA37C758D0D8CD3AE9740C466F75C6183FF5E93D',
       include_src => false,
     }
   }
